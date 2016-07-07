2ca7dfdf29768670b4639699ebfe4296596761fb
Julien Perret <julien.perret@gmail.com>
2016-07-07 14:16:41 +0200

    finished full simple lunr example

---
+++ demo/src/main/resources/js/index.js
@@ -0,0 +1,9 @@
+var index = [{
+  "url": "https://ccl.northwestern.edu/netlogo/",
+  "title": "NetLogo is not that bad",
+  "body": "NetLogo blablabla."
+}, {
+  "url": "http://openmole.org",
+  "title": "OpenMole is cool",
+  "body": "OpenMole blablabla."
+}]
--- demo/src/main/scala/fr/iscpif/demo/LunrDemo.scala
+++ demo/src/main/scala/fr/iscpif/demo/LunrDemo.scala
@@ -1,12 +1,13 @@
 package demo
 
-import fr.iscpif.scaladget.mapping.lunr.{IIndexSearchResult, Index, Importedjs}
+import fr.iscpif.scaladget.mapping.lunr.{IIndexSearchResult, Importedjs, Index}
 import fr.iscpif.scaladget.tools.JsRxTags._
+import org.scalajs.dom
 import rx._
-import scala.scalajs.js.Dynamic.{literal ⇒ lit}
 
-
-import scala.scalajs.js.{JSON, JSApp}
+import scala.scalajs.js
+import scala.scalajs.js.Dynamic.{literal => lit}
+import scala.scalajs.js.{JSApp, JSON}
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom.tags
 import scalatags.JsDom.all._
@@ -28,7 +29,7 @@ import scalatags.JsDom.all._
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-@JSExport("LunrDemo")
+/*@JSExport("LunrDemo")
 object LunrDemo extends JSApp {
 
   @JSExport
@@ -74,5 +75,55 @@ object LunrDemo extends JSApp {
       }
 
     ).render)
+  }
+
+  case class Page(url: String, title: String, body: String)
+
+}*/
+
+
+@JSExport
+object LunrDemo {
+  val searchInput = input(placeholder:="Search").render
+  val lunrIndex: Var[Option[Index]] = Var(None)
+  val results: Var[Seq[IIndexSearchResult]] = Var(Seq())
+  @JSExport
+  def loadIndex(indexArray:js.Array[js.Any]): Unit = {
+    val index = Importedjs.lunr((i: Index) => {
+      i.field("title", lit("boost" → 10).value)
+      i.field("body", lit("boost" → 1).value)
+      i.ref("url")
+      indexArray.foreach(p => {
+        i.add(p)
+      })
+    })
+    lunrIndex() = Some(index)
+    val resultList = tags.div(
+      color := "white",
+      Rx {
+        for {r <- results()} yield {
+          tags.div(
+            tags.span(
+              tags.a(r.ref, cursor := "pointer", href := r.ref, target:="_blank"),
+              r.score
+            )
+          )
+        }
+      }
+    ).render
+    dom.document.body.appendChild(
+      tags.div(
+      form(`type`:="submit", searchInput, onsubmit:={()=>
+      search()
+      false
+      }), resultList).render)
+  }
+
+  @JSExport
+  def search(): Unit = {
+    println("search")
+    results() = lunrIndex.now.map { i =>
+      i.search(searchInput.value).toSeq
+    }.getOrElse(Seq())
   }
 }
