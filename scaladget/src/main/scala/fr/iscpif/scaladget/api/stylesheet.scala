a6f592ee7c5eae2d9502c6eb06ba2a454fd71ec7
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2016-12-23 19:09:46 +0100

    Add glyphs

--- scaladget/src/main/scala/fr/iscpif/scaladget/api/stylesheet.scala
+++ scaladget/src/main/scala/fr/iscpif/scaladget/api/stylesheet.scala
@@ -231,6 +231,8 @@ package bootstrap {
     lazy val glyph_alph_sorting: Glyphicon = toGlyphicon("glyphicon-sort-by-alphabet")
     lazy val glyph_triangle_bottom: Glyphicon = toGlyphicon("glyphicon-triangle-bottom")
     lazy val glyph_triangle_top: Glyphicon = toGlyphicon("glyphicon-triangle-top")
+    lazy val glyph_chevron_left: Glyphicon = toGlyphicon("glyphicon glyphicon-chevron-left")
+    lazy val glyph_chevron_rightt: Glyphicon = toGlyphicon("glyphicon glyphicon-chevron-right")
     lazy val caret: ModifierSeq = toClass("caret")
 
     //NAVBARS
