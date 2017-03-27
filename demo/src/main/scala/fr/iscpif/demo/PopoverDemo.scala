7a8b173774b0772174dcddc8ede1212821119dfd
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2017-03-27 11:06:41 +0200

    Demo layout fixes

--- demo/src/main/scala/fr/iscpif/demo/PopoverDemo.scala
+++ demo/src/main/scala/fr/iscpif/demo/PopoverDemo.scala
@@ -30,28 +30,18 @@ object PopoverDemo extends Demo {
   val sc = sourcecode.Text {
 
     import fr.iscpif.scaladget.api.Popup._
-    import fr.iscpif.scaladget.tools.JsRxTags._
-    import rx._
 
     val buttonStyle: ModifierSeq = Seq(
       btn_default,
       sheet.marginRight(5)
     )
 
-
-    val newPop: Var[Option[Popover]] = Var(None)
-
     div(
-      bs.button("Build new Popover", buttonStyle, ()=> { newPop() = Some(new Popover(bs.button("NEW Pop !", buttonStyle, ()=> {}),
-        "Pop life", dismissible = true, title = Some("My title")))}),
       bs.button("Left", buttonStyle, () => {}).popover("Popover on hover on left", Left),
       bs.button("Title", buttonStyle, () => {}).popover("Popover on hover with Title", Top, title = Some("Pop title")),
       bs.button("Dismissable", buttonStyle, () => {}).popover("Dismissible Popover on hover with Title", Top, HoverPopup, Some("Pop title"), true),
       bs.button("Left (click)", buttonStyle, () => {}).popover("Popover on click on right", Left, ClickPopup),
-      bs.input("")(width := 320, sheet.marginTop(10), placeholder := "Bottom (click)").popover("Tooltip on click on bottom", Bottom, ClickPopup),
-      Rx {
-        newPop().map{_.render}.getOrElse(div("Nothing").render)
-      }
+      bs.input("")(width := 320, sheet.marginTop(10), placeholder := "Bottom (click)").popover("Tooltip on click on bottom", Bottom, ClickPopup)
     ).render
   }
 
