355027383e6b8edd2f44ab27240b240a85619dd4
Mathieu <mathieu.leclaire@iscpif.fr>
2020-06-17 13:20:37 +0200

    Fix highlightBlock type

--- highlightjs/src/main/scala/scaladget/highlightjs/HighlightJS.scala
+++ highlightjs/src/main/scala/scaladget/highlightjs/HighlightJS.scala
@@ -55,7 +55,7 @@ trait HighlightStatic extends js.Object {
     * This function is the one to use to apply highlighting dynamically after page load or within initialization code of third-party Javascript frameworks.
     * The function uses language detection by default but you can specify the language in the class attribute of the DOM node. See the class reference for all available language names and aliases.
     */
-  def highlightBlock(el: Element) = js.native
+  def highlightBlock(el: Element):Unit = js.native
 
   /**
     * Post-processing of the highlighted markup. Currently consists of replacing indentation TAB characters and using <br> tags instead of new-line characters. Options are set globally with configure.
